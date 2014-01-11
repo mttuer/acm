1eeeed7a5173367ed53376fb1e1162b8111a6018
Mike Tuer <mttuer@mtu.edu>
2014-01-11 16:26:25 -0500

    Remove .DS_Store

--- app/views.py
+++ app/views.py
@@ -7,9 +7,9 @@ import copy
 @app.route('/index')
 def index():
     links = [
-            {'title': 'Home', 'view': "home"},
-            {'title': 'links', 'view': "home"},
-            {'title': 'Interviews', 'view': "home"},
+            {'title': 'Home', 'view': "index"},
+            {'title': 'Events', 'view': "events"},
+            {'title': 'Admin', 'view': "admin"},
             ]
     user = {'nickname': 'Miguel'}
     return render_template("index.html", 
@@ -18,27 +18,48 @@ def index():
             user = user
             )
 
+@app.route('/events')
+def events():
+    db = connect.getDB()
+
+    tbName = "tbEvents"
+    keys = connect.getColNames(tbName)
+    kToI = dict()
+    for i in range(0,len(keys)):
+        kToI[keys[i]] = i
+    getRowsForKeys(tbName, keys, db)
+    tbl = {'keys':keys, 'rows':copy.deepcopy(db.fetchall())}
+
+    links = [
+            {'title': 'Home', 'view': "index"},
+            {'title': 'Events', 'view': "events"},
+            ]
+
+    user = {'nickname': 'Miguel'}
+
+    return render_template("events.html", 
+            title = 'ACM@MichiganTech', 
+            links = links, 
+            user = user,
+            tbl = tbl,
+            kToI=kToI
+            )
+
+
 @app.route('/admin')
 def admin():
     db = connect.getDB()
     tables = ["tbUsers","tbPosts","tbEvents"]
+
     tblSets = list()
     for tbl in tables:
         keys = connect.getColNames(tbl)
-        s = str()
-        for k in keys:
-            s += k + ","
-        s="SELECT " + s[:-1] + " FROM " + tbl
-        print s
-        db.execute(s)
-        tblSets.append({'tbName':tbl,
-            'keys':keys,
+        getRowsForKeys(tbl, keys, db)
+        tblSets.append({'tbName':tbl, 'keys':keys,
             'rows':copy.deepcopy(db.fetchall())})
 
     links = [
-            {'title': 'Home', 'view': "home"},
-            {'title': 'links', 'view': "home"},
-            {'title': 'Interviews', 'view': "home"},
+            {'title': 'Home', 'view': "index"},
             ]
 
     user = {'nickname': 'Miguel'}
@@ -49,4 +70,11 @@ def admin():
             user = user,
             tables=tblSets
             )
+
+def getRowsForKeys(tbl, keys, db):
+    s = str()
+    for k in keys:
+        s += k + ","
+    s="SELECT " + s[:-1] + " FROM " + tbl
+    db.execute(s)
 
